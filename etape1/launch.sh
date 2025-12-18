1742dc2f59915ae829d946fdb357b98e70260bf3
Sebastien <sgutierrezpe@icloud.com>
2025-12-18 11:37:02 +0100

    Etape1

--- etape1/launch.sh
+++ etape1/launch.sh
@@ -1,1 +1,20 @@
-t
+#!/bin/bash
+
+docker rm -f script 2>/dev/null || true
+docker rm -f http 2>/dev/null || true
+docker network rm tp3 2>/dev/null || true
+docker network create tp3
+
+docker container run -d \
+  --name script \
+  --network tp3 \
+  -v "$(pwd)/src":/app \
+  php:8.2-fpm
+
+docker container run -d \
+  --name http \
+  --network tp3 \
+  -p 8080:80 \
+  -v "$(pwd)/src":/app \
+  -v "$(pwd)/config/default.conf":/etc/nginx/conf.d/default.conf \
+  nginx
